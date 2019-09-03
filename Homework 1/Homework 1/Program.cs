4d4b8a0bcb321314b5589739764866d1394175e8
Don-Vincent Snell <39629030+DVSnell@users.noreply.github.com>
2019-09-03 01:36:07 -0600

    Initial Repo + Homework 1 Start

---
+++ Homework 1/Homework 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System; //Imports (libraries)
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playtime_101 //groups classes together for duplicate namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args) // first function, ran first, "void" = no return
+        {
+            int score = 0;
+            const int myAge = 34;
+            string playerName = "Name";
+            string ageIn = "0";
+            int ageOut = 0;
+            int ageSum = 0;
+            string favGame = "Game";
+
+            Console.WriteLine("Greetings.  By what name do you wish to be called?");
+            playerName = Console.ReadLine();
+
+            Console.WriteLine("It is a pleasure to meet you " + playerName + ".");
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("I hope you don't find it to forward of me, but I must inquire as to your age.");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("How old are you?");
+            ageIn = Console.ReadLine();
+            Int32.TryParse(ageIn, out ageOut);
+            System.Threading.Thread.Sleep(1000);
+            ageSum = myAge + ageOut;
+
+            Console.WriteLine("Fantastic.");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Did you know that if we add our ages together we get " + ageSum + "?");
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("I like to play games.  What is your favorite game?");
+            favGame = Console.ReadLine();
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(favGame +" sounds like an intersting game.  You'll have to tell me more about it later, but right now I'm wondering if you'd like to play a different game.  Would you like to play a math game with me?");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mad Lab/Mad Lab/Mad Lab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System; //Imports (libraries)
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playtime_101 //groups classes together for duplicate namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args) // first function, ran first, "void" = no return
+        {
+            int score = 0;
+            double decimals = 0.0;
+            string name = "Floop Greebyg";
+            string bananaState = "Yes";
+
+            Console.WriteLine("Please tell me your name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("It is a pleasure to meet you " + name + ". My name is Don");
+            Console.WriteLine("Do you like bananas?");
+            bananaState = Console.ReadLine();
+
+            Console.WriteLine("That's super interesting.");
+            Console.WriteLine("Did you know I once saw a banana kill a man in Vegas."); // Writes to a line and adds return
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("He had it coming.");
+            Console.ReadLine();
+        }
+    }
+}
