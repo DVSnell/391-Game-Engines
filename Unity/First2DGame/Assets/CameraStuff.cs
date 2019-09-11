5e15861a3517b91054d1d930917d51184f75709b
Don-Vincent Snell <39629030+DVSnell@users.noreply.github.com>
2019-09-11 01:10:04 -0600

    Fix?

---
+++ Unity/First2DGame/Assets/CameraStuff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets;
+
+public class CameraStuff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("In Start"); ;
+        Dog dog = new Dog("Doggie");
+        print(dog.Name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            print("d was pressed");
+        }
+        //print("In Update");
+    }
+}
